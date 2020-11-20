499ab7350c46f08b059912f4e2d891b6d81043bf
juniorcintra <junior.developer@outlook.com.br>
2020-11-20 03:33:10 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main (){
+
+  runApp(MaterialApp(
+    title: "Thinking Out Box",
+    home: Column(
+      children: <Widget>[
+      Text(
+          "Texto 1",
+        style: TextStyle(
+
+        ),
+      ),
+    ],),
+    //home: Container(color: Colors.white,),
+  ));
+}
